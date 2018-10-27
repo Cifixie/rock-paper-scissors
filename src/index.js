a857c5b613478fb9a28a2a908d160406276ae6a7
Tommi <mato12@hotmail.fi>
2018-10-27 11:04:36 +0300

    simple rock-paper-scissors game

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import { add } from "ramda";
-const options = ["rock", "paper", "scissors"];
 
-export default add;
+const r = {
+  win: 1,
+  draw: 0,
+  loss: -1
+};
+
+const result = [
+  [r.draw, r.loss, r.win],
+  [r.win, r.draw, r.loss],
+  [r.loss, r.win, r.draw]
+];
+
+export const options = {
+  rock: 0,
+  paper: 1,
+  scissors: 2
+};
+
+export default (a, b) => result[a][b];
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,22 @@
-import sum from "./index";
+import rps, { options, results } from "./index";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test("equal options is always draw", () => {
+  expect(rps(options.rock, options.rock)).toBe(0);
+  expect(rps(options.paper, options.paper)).toBe(0);
+  expect(rps(options.scissors, options.scissors)).toBe(0);
+});
+
+test("rock beats scissors but losses to paper", () => {
+  expect(rps(options.rock, options.scissors)).toBe(1);
+  expect(rps(options.rock, options.paper)).toBe(-1);
+});
+
+test("paper beats rock but losses to scissors", () => {
+  expect(rps(options.paper, options.rock)).toBe(1);
+  expect(rps(options.paper, options.scissors)).toBe(-1);
+});
+
+test("scissors beats paper but losses to rock", () => {
+  expect(rps(options.scissors, options.paper)).toBe(1);
+  expect(rps(options.scissors, options.rock)).toBe(-1);
 });
